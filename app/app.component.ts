4b011a0ca82f89a25b10968c1e06f8c450c0f629
Luis Fernando Manga <luisfernandomanga@gmail.com>
2016-11-16 08:42:58 -0200

    commit

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId : module.id,
+  selector: 'my-app',
+  template: '<home></home>'
+})
+export class AppComponent { }
---
+++ app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule  } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent, HomeComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
